f4aefb2a3ff60f007127553f1a682792e3964afb
Kim-DaeHan <Kim-DaeHan@users.noreply.github.com>
2023-12-05 11:57:36 +0900

    .

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   console.log("start");
+  console.log("test");
+  const mongodbUri = process.env.MONGODB_URI;
+  console.log(`Connected to MongoDB at: ${mongodbUri}`);
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
