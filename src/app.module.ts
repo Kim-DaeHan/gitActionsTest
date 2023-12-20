a5f941ad1b06e46e2748ef33d9bd1de52e8aaba4
Kim-DaeHan <Kim-DaeHan@users.noreply.github.com>
2023-12-20 10:31:48 +0900

    ..

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,14 +5,14 @@ import { UserModule } from "./user/user.module";
 
 @Module({
   imports: [
-    MongooseModule.forRoot(process.env.MONGODB_URI),
+    MongooseModule.forRoot("mongodb://192.168.0.2:27017/"),
     TestModule,
     UserModule,
   ],
 })
 export class AppModule {
   constructor() {
-    const mongodbUri = process.env.MONGODB_URI;
+    const mongodbUri = "mongodb://192.168.0.2:27017/ 2222";
     console.log(`Connected to MongoDB at: ${mongodbUri}`);
     // 여기에서 mongodbUri를 사용하여 MongoDB에 연결하는 로직을 구현할 수 있습니다.
   }
